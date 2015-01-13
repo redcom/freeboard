4cadb69085ccf660bf1e4dd601e5c430e67b7fb7
tanaka0323 <tanaka@infocorpus.co.jp>
2015-01-13 19:23:26 +0900

    Google map style removed. weather2.json example added.

--- js/freeboard.plugins.js
+++ js/freeboard.plugins.js
@@ -1586,63 +1586,7 @@
 					zoom: 13,
 					center: new google.maps.LatLng(37.235, -115.811111),
 					disableDefaultUI: true,
-					draggable: false,
-					styles: [
-						{"featureType": "water", "elementType": "geometry", "stylers": [
-							{"color": "#2a2a2a"}
-						]},
-						{"featureType": "landscape", "elementType": "geometry", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 20}
-						]},
-						{"featureType": "road.highway", "elementType": "geometry.fill", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 17}
-						]},
-						{"featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 29},
-							{"weight": 0.2}
-						]},
-						{"featureType": "road.arterial", "elementType": "geometry", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 18}
-						]},
-						{"featureType": "road.local", "elementType": "geometry", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 16}
-						]},
-						{"featureType": "poi", "elementType": "geometry", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 21}
-						]},
-						{"elementType": "labels.text.stroke", "stylers": [
-							{"visibility": "on"},
-							{"color": "#000000"},
-							{"lightness": 16}
-						]},
-						{"elementType": "labels.text.fill", "stylers": [
-							{"saturation": 36},
-							{"color": "#000000"},
-							{"lightness": 40}
-						]},
-						{"elementType": "labels.icon", "stylers": [
-							{"visibility": "off"}
-						]},
-						{"featureType": "transit", "elementType": "geometry", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 19}
-						]},
-						{"featureType": "administrative", "elementType": "geometry.fill", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 20}
-						]},
-						{"featureType": "administrative", "elementType": "geometry.stroke", "stylers": [
-							{"color": "#000000"},
-							{"lightness": 17},
-							{"weight": 1.2}
-						]}
-					]
+					draggable: false
 				};
 
 				map = new google.maps.Map(element, mapOptions);
