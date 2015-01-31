11b93b548c90e7b103ca1cc35997f5af7fe00cbb
tanaka0323 <tanaka@infocorpus.co.jp>
2015-01-31 12:19:45 +0900

    Fxied date.toLocaleString function for other browsers and format are different problems in a Japanese environment IE9 ~ 11
    Example:
    "2014年1月28日 13時13分13秒" IE (Win)
    "2014/1/28 13:13:13" other browser (Win Mac)

--- plugins/freeboard/freeboard.datasources.js
+++ plugins/freeboard/freeboard.datasources.js
@@ -28,14 +28,32 @@
 			timer = setInterval(self.updateNow, currentSettings.refresh * 1000);
 		}
 
+		function formatDate(date, format) {
+			if (!format)
+				format = 'YYYY/MM/DD hh:mm:ss';
+			format = format.replace(/YYYY/g, date.getFullYear());
+			format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+			format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
+			format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
+			format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+			format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+			if (format.match(/S/g)) {
+				var milliSeconds = ('00' + date.getMilliseconds()).slice(-3);
+				var length = format.match(/S/g).length;
+				for (var i = 0; i < length; i++)
+					format = format.replace(/S/, milliSeconds.substring(i, i + 1));
+			}
+			return format;
+		}
+
 		this.updateNow = function () {
 			var date = new Date();
 
 			var data = {
 				numeric_value: date.getTime(),
-				full_string_value: date.toLocaleString(),
-				date_string_value: date.toLocaleDateString(),
-				time_string_value: date.toLocaleTimeString(),
+				full_string_value: formatDate(date),
+				date_string_value: formatDate(date, 'YYYY/MM/DD'),
+				time_string_value: formatDate(date, 'hh:mm:ss'),
 				date_object: date
 			};
 
